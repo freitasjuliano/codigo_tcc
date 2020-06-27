b850ee1b49f9fcd104f21427b71d9a07d0bc92de
Juliano de Freitas <57322770+offlines@users.noreply.github.com>
2020-06-27 04:01:31 -0300

    Add files via upload

---
+++ js/swapping.js
@@ -0,0 +1,132 @@
+function change_text() {
+    document.getElementById("title_swap").innerHTML = "Di Cavalcanti";
+    document.getElementById("info_swap").innerHTML = "Documentário - 18 min - Glauber Rocha";
+    document.getElementById("text_swap").innerHTML = "Após ouvir no rádio uma notícia sobre a morte de Di Cavalcanti, o cineasta Glauber Rocha reuniu uma pequena equipe de filmagem para registrar o velório e o enterro do pintor...";
+    document.getElementById('image_swap').src="../images/di.jpg";
+}
+
+function change_text2() {
+    document.getElementById("title_swap").innerHTML = "Fim de Festa";
+    document.getElementById("info_swap").innerHTML = "Drama - 1h34 - Hilton Lacerda";
+    document.getElementById("text_swap").innerHTML = "Após ouvir no rádio uma notícia sobre a morte de Di Cavalcanti, o cineasta Glauber Rocha reuniu uma pequena equipe de filmagem para registrar o velório e o enterro do pintor...";
+    document.getElementById('image_swap').src="../images/fim.png";
+}
+
+function change_text3() {
+    document.getElementById("title_swap").innerHTML = "Blábláblá";
+    document.getElementById("info_swap").innerHTML = "Drama - 28min - Andrea Tonacci";
+    document.getElementById("text_swap").innerHTML = "Brasil, final da década de 1968. Após o golpe militar que pôs a democracia brasileira em suspensão, uma ditadura é instaurada no país, reprimindo e oprimindo seus adversários, gerando uma atmosfera de tensão da qual ninguém poderia sair ileso...";
+    document.getElementById('image_swap').src="../images/bla.png";
+}
+
+function change_text4() {
+    document.getElementById("title_swap").innerHTML = "SuperOutro";
+    document.getElementById("info_swap").innerHTML = "Drama - 48min - Edgar Navarro";
+    document.getElementById("text_swap").innerHTML = "Um louco e pobre mendigo jogado nas ruas de Salvador tenta, de todas as maneiras, libertar a si mesmo da miséria que o acompanha durante toda sua vida, realizando atos tresloucados pela cidade.";
+    document.getElementById('image_swap').src="../images/super.png";
+}
+
+function change_text5() {
+    document.getElementById("title_swap").innerHTML = "O Beijo no Asfalto";
+    document.getElementById("info_swap").innerHTML = "Drama - 1h38 - Murilo Benício";
+    document.getElementById("text_swap").innerHTML = "Ao presenciar um atropelamento, o bancário Arandir atende ao pedido de um beijo na boca feito pelo sujeito prestes a morrer.";
+    document.getElementById('image_swap').src="../images/bjo.png";
+}
+
+function change_text6() {
+    document.getElementById("title_swap").innerHTML = "Alma no olho";
+    document.getElementById("info_swap").innerHTML = "Drama - 11min - Zózimo Bulbul";
+    document.getElementById("text_swap").innerHTML = "Metáfora sobre a escravidão e a busca da liberdade através da transformação interna do ser, num jogo de imagens de inspiração concretista.";
+    document.getElementById('image_swap').src="../images/alma.png";
+}
+
+function change_text7() {
+    document.getElementById("title_swap").innerHTML = "Vinil Verde";
+    document.getElementById("info_swap").innerHTML = "Suspende/Fantasia - 16min - Kleber Mendonça Filho";
+    document.getElementById("text_swap").innerHTML = "Uma menina é presenteada com vinis coloridos. Deslumbrada com as cores, encanta-se quando escuta as músicas na vitrola. Porém, ela é proibida de ouvir o vinil verde, o que mais desperta a sua curiosidade.";
+    document.getElementById('image_swap').src="../images/vinil.png";
+}
+
+function change_text8() {
+    document.getElementById("title_swap").innerHTML = "Macabro";
+    document.getElementById("info_swap").innerHTML = "Suspense - 1h38 - Marcos Prado";
+    document.getElementById("text_swap").innerHTML = "Na década de 90, dois irmãos são acusados de assassinarem oito mulheres, um homem e uma criança de forma brutal na região da Serra dos Órgãos.";
+    document.getElementById('image_swap').src="../images/macabro.png";
+}
+
+function change_text9() {
+    document.getElementById("title_swap").innerHTML = "Recife frio";
+    document.getElementById("info_swap").innerHTML = "ficção - 24min - Kleber Mendonça Filho";
+    document.getElementById("text_swap").innerHTML = "Um meteorito cai sobre Recife e a cidade torna-se fria, com baixas temperaturas. A região sofre com os efeitos deste acontecimento sobrenatural, e deixa de ser tropical para incluir até pinguins em sua fauna.";
+    document.getElementById('image_swap').src="../images/recife.png";
+}
+
+function change_text10() {
+    document.getElementById("title_swap").innerHTML = "Guaxuma";
+    document.getElementById("info_swap").innerHTML = "Animação - 15min - Nara Normande";
+    document.getElementById("text_swap").innerHTML = "A amizade e o amor de infância entre Nara e Tayra, criadas na região litorânea de Alagoas.";
+    document.getElementById('image_swap').src="../images/gua.png";
+}
+
+function change_text11() {
+    document.getElementById("title_swap").innerHTML = "Breve Miragem do Sol";
+    document.getElementById("info_swap").innerHTML = "Drama - 1h38 - Eryk Rocha";
+    document.getElementById("text_swap").innerHTML = "Vivendo à própria sorte e recentemente divorciado, Paulo começa a trabalhar como taxista durante a noite, no Rio de Janeiro.";
+    document.getElementById('image_swap').src="../images/sol.png";
+}
+
+function change_text12() {
+    document.getElementById("title_swap").innerHTML = "Eletrodoméstica";
+    document.getElementById("info_swap").innerHTML = "Drama - 22min - Kleber Mendonça Filho";
+    document.getElementById("text_swap").innerHTML = "No dia em que chega uma TV de 29 polegadas à casa de uma família pernambucana, a dona de casa se ocupa com tarefas corriqueiras, enquanto espera ansiosamente pelo fim do trabalho da máquina de lavar.";
+    document.getElementById('image_swap').src="../images/eletro.png";
+}
+
+function change_text13() {
+    document.getElementById("title_swap").innerHTML = "Quintal";
+    document.getElementById("info_swap").innerHTML = "Drama - 20min - André Novais Oliveira";
+    document.getElementById("text_swap").innerHTML = "O filme retrata um dia na vida de um casal de idosos em Contagem, Região Metropolitana de Belo Horizonte. Os pais do diretor são os atores principais deste curta.";
+    document.getElementById('image_swap').src="../images/quintal.png";
+}
+
+function change_text14() {
+    document.getElementById("title_swap").innerHTML = "Dias de Greve";
+    document.getElementById("info_swap").innerHTML = "Ficção - 24min - Adirley Queirós";
+    document.getElementById("text_swap").innerHTML = "Uma greve de serralheiros é deflagrada em Ceilândia (DF), cidade próxima à Capital Federal.";
+    document.getElementById('image_swap').src="../images/quintal.png";
+}
+
+function change_text15() {
+    document.getElementById("title_swap").innerHTML = "Estado Itinerante";
+    document.getElementById("info_swap").innerHTML = "Drama - 25min - Ana Carolina Soares";
+    document.getElementById("text_swap").innerHTML = "Vivi, uma cobradora de ônibus, quer escapar de uma relação opressora. Entre as paradas no ponto final, os encontros com outras mulheres fortalecem a trabalhadora e seu desejo de fuga.";
+    document.getElementById('image_swap').src="../images/estado.png";
+}
+
+function change_text16() {
+    document.getElementById("title_swap").innerHTML = "Marighella";
+    document.getElementById("info_swap").innerHTML = "Drama - 2h35 - Wagner Moura";
+    document.getElementById("text_swap").innerHTML = "Conheça a história de Carlos Marighella, assassinado pela ditadura militar e que deixou um legado que se estendeu por diversas gerações.";
+    document.getElementById('image_swap').src="../images/mari.png";
+}
+
+function change_text17() {
+    document.getElementById("title_swap").innerHTML = "Torre";
+    document.getElementById("info_swap").innerHTML = "Aventura - 19min - Nádia Mangolini";
+    document.getElementById("text_swap").innerHTML = "Quatro irmãos, filhos de Virgílio Gomes da Silva, o primeiro desaparecido político da ditadura militar brasileira, relatam suas infâncias durante o regime.";
+    document.getElementById('image_swap').src="../images/torre.png";
+}
+
+function change_text18() {
+    document.getElementById("title_swap").innerHTML = "Guida";
+    document.getElementById("info_swap").innerHTML = "Drama - 12min - Nádia Rosana Urbes";
+    document.getElementById("text_swap").innerHTML = "Uma senhora que há anos trabalha como arquivista, tem sua rotina modificada quando decide posar como modelo vivo.";
+    document.getElementById('image_swap').src="../images/guida.png";
+}
+
+function change_text19() {
+    document.getElementById("title_swap").innerHTML = "Três Verões";
+    document.getElementById("info_swap").innerHTML = "Drama/Comédia - 1h34 - Sandra Kogut";
+    document.getElementById("text_swap").innerHTML = " A cada verão, entre Natal e Ano Novo, o casal Edgar e Marta recebe amigos e família na sua mansão à beira mar. Em 2015 tudo parece ir bem, mas em 2016 a mesma festa é cancelada.  Madá, a empregada responsável por toda a administração doméstica, vivência a decadência do casal, vítimas do sonho neoliberal.";
+    document.getElementById('image_swap').src="../images/tres.png";
+}
